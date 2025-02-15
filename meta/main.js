2494318a8dffa70b8e666eeb78bb1bab5962f7c0
Mingrui-Zhuang <m1zhuang@ucsd.edu>
2025-02-14 18:15:25 -0800

    1815

--- meta/main.js
+++ meta/main.js
@@ -1,21 +1,156 @@
 let data = [];
+let commits = [];
 
 async function loadData() {
-  data = await d3.csv('../gitignore/loc.csv');
-  console.log(data);
+  data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+
+  displayStats();
+  createScatterplot();
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-  await loadData();
-});
+function processCommits() {
+  commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/YOUR_REPO/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
 
-async function loadData() {
-    data = await d3.csv('loc.csv', (row) => ({
-      ...row,
-      line: Number(row.line), // or just +row.line
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + 'T00:00' + row.timezone),
-      datetime: new Date(row.datetime),
-    }));
-  }
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        configurable: false,
+        writable: false,
+        enumerable: false,
+      });
+
+      return ret;
+    });
+}
+
+function displayStats() {
+  // Process commits first
+  processCommits();
+
+  // Add the title above the dl element
+  d3.select('#stats').append('h2').text('Summary');
+
+  // Create the dl element
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  // Add total commits
+  dl.append('dt').text('Total Commits');
+  dl.append('dd').text(commits.length);
+
+  // Add total LOC
+  dl.append('dt').html('Total LOC');
+  dl.append('dd').text(data.length);
+
+  // Add more stats as needed...
+  // Number of files in the codebase
+  const numFiles = d3.rollups(data, (v) => v.length, (d) => d.file).length;
+  dl.append('dt').text('Number of Files');
+  dl.append('dd').text(numFiles);
+
+  // Maximum file length (in lines)
+  const maxFileLength = d3.max(data, (d) => d.length);
+  dl.append('dt').text('Max File Length');
+  dl.append('dd').text(maxFileLength);
+
+  // Longest line length
+  const longestLineLength = d3.max(data, (d) => d.line);
+  dl.append('dt').text('Longest Line Length');
+  dl.append('dd').text(longestLineLength);
+
+  // Day of the week that most work is done
+  const workByDay = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).toLocaleString('en', { weekday: 'long' })
+  );
+  const mostWorkDay = d3.max(workByDay, (d) => d[1]);
+  const dayWithMostWork = workByDay.find((d) => d[1] === mostWorkDay)[0];
+  dl.append('dt').text('Day with most work');
+  dl.append('dd').text(dayWithMostWork);
+}
+
+
+function createScatterplot() {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 50 };
+
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  // Create the SVG
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  // Create scales
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([usableArea.left, usableArea.right])
+    .nice();
+
+  const yScale = d3.scaleLinear().domain([0, 24]).range([usableArea.bottom, usableArea.top]);
+
+  // Create the axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale).tickFormat(
+    (d) => String(d % 24).padStart(2, '0') + ':00'
+  );
+
+  // Add X axis
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  // Add Y axis
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
+  // Draw the scatter plot
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}
+
+// Call the loadData function to test it out
+loadData();
